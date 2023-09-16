4c840fd51bd7122625018be78725ecf013f5d02c
TranThangBin <thangdev04@gmail.com>
2023-09-16 10:16:03 +0700

    feat: lspoverload + copy to clipboard

--- lua/tranquangthang/remap.lua
+++ lua/tranquangthang/remap.lua
@@ -1,4 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
-vim.keymap.set("i", "jj", "<Esc>")
+
+vim.keymap.set("n", "<leader>y", '"+y$')
+vim.keymap.set("v", "<leader>y", '"+y')
+
 vim.keymap.set("s", "jj", "<Esc>")
+vim.keymap.set("i", "jj", "<Esc>")
