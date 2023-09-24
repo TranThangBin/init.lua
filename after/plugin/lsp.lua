94d40e0ef2aa8a305189560b145f42d32b199d84
TranThangBin <thangdev04@gmail.com>
2023-09-24 22:29:38 +0700

    feat: more lsp configure and toggle text wrap in key remap

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,4 @@
 local lsp = require("lsp-zero")
-local lspconfig = require("lspconfig")
 
 lsp.preset("recommended")
 
@@ -47,6 +46,37 @@ lsp.set_preferences({
 lsp.skip_server_setup({ "eslint" })
 
 lsp.on_attach(function(client, bufnr)
+	local opts = { buffer = bufnr, remap = false }
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>vws", function()
+		vim.lsp.buf.workspace_symbol()
+	end, opts)
+	vim.keymap.set("n", "<leader>vd", function()
+		vim.lsp.buf.open_float()
+	end, opts)
+	vim.keymap.set("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	vim.keymap.set("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end, opts)
+	vim.keymap.set("n", "<leader>vca", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrr", function()
+		vim.lsp.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrn", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
 	--- Guard against servers without the signatureHelper capability
 	if client.server_capabilities.signatureHelpProvider then
 		require("lsp-overloads").setup(client, {
--- lua/tranquangthang/remap.lua
+++ lua/tranquangthang/remap.lua
@@ -10,9 +10,5 @@ vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("x", "<leader>p", '"_dP')
 
 vim.keymap.set("n", "<leader>w", function()
-	if vim.wo.wrap then
-		vim.wo.wrap = false
-	else
-		vim.wo.wrap = true
-	end
+	vim.wo.wrap = not vim.wo.wrap
 end)
