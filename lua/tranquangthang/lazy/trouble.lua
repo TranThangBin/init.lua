bdeaeea29d5fe023731cfa3aeff2ab378b45dcb4
TranThangBin <thangdev04@gmail.com>
2024-06-01 21:18:46 +0700

    feat(migration): migrate trouble.nvim configuration

--- lua/tranquangthang/lazy/trouble.lua
+++ lua/tranquangthang/lazy/trouble.lua
@@ -1,102 +1,190 @@
 return {
-	"folke/trouble.nvim",
+    "folke/trouble.nvim",
 
-	dependencies = "nvim-tree/nvim-web-devicons",
+    dependencies = "nvim-tree/nvim-web-devicons",
 
-	opts = {
-		position = "bottom",
-		height = 10,
-		width = 50,
-		icons = true,
-		mode = "workspace_diagnostics",
-		severity = nil,
-		fold_open = "",
-		fold_closed = "",
-		group = true,
-		padding = true,
-		cycle_results = true,
-		action_keys = {
-			close = "q",
-			cancel = "<esc>",
-			refresh = "r",
-			jump = {
-				"<cr>",
-				"<tab>",
-				"<2-leftmouse>",
-			},
-			open_split = { "<c-x>" },
-			open_vsplit = { "<c-v>" },
-			open_tab = { "<c-t>" },
-			jump_close = { "o" },
-			toggle_mode = "m",
-			switch_severity = "s",
-			toggle_preview = "P",
-			hover = "K",
-			preview = "p",
-			open_code_href = "c",
-			close_folds = { "zM", "zm" },
-			open_folds = { "zR", "zr" },
-			toggle_fold = { "zA", "za" },
-			previous = "k",
-			next = "j",
-			help = "?",
-		},
-		multiline = true,
-		indent_lines = true,
-		win_config = { border = "single" },
-		auto_open = false,
-		auto_close = false,
-		auto_preview = true,
-		auto_fold = false,
-		auto_jump = { "lsp_definitions" },
-		include_declaration = {
-			"lsp_references",
-			"lsp_implementations",
-			"lsp_definitions",
-		},
-		signs = {
-			error = "",
-			warning = "",
-			hint = "",
-			information = "",
-			other = "",
-		},
-		use_diagnostic_signs = false,
-	},
+    opts = {
+        auto_close = false,
+        auto_open = false,
+        auto_preview = true,
+        auto_refresh = true,
+        auto_jump = false,
+        focus = false,
+        restore = true,
+        follow = true,
+        indent_guides = true,
+        max_items = 200,
+        multiline = true,
+        pinned = false,
+        warn_no_results = true,
+        open_no_results = false,
+        win = {},
+        preview = {
+            type = "main",
+            scratch = true,
+        },
+        throttle = {
+            refresh = 20,
+            update = 10,
+            render = 10,
+            follow = 100,
+            preview = { ms = 100, debounce = true },
+        },
+        keys = {
+            ["?"] = "help",
+            r = "refresh",
+            R = "toggle_refresh",
+            q = "close",
+            o = "jump_close",
+            ["<esc>"] = "cancel",
+            ["<cr>"] = "jump",
+            ["<2-leftmouse>"] = "jump",
+            ["<c-s>"] = "jump_split",
+            ["<c-v>"] = "jump_vsplit",
+            ["}"] = "next",
+            ["]]"] = "next",
+            ["{"] = "prev",
+            ["[["] = "prev",
+            i = "inspect",
+            p = "preview",
+            P = "toggle_preview",
+            zo = "fold_open",
+            zO = "fold_open_recursive",
+            zc = "fold_close",
+            zC = "fold_close_recursive",
+            za = "fold_toggle",
+            zA = "fold_toggle_recursive",
+            zm = "fold_more",
+            zM = "fold_close_all",
+            zr = "fold_reduce",
+            zR = "fold_open_all",
+            zx = "fold_update",
+            zX = "fold_update_all",
+            zn = "fold_disable",
+            zN = "fold_enable",
+            zi = "fold_toggle_enable",
+            gb = {
+                action = function(view)
+                    view:filter({ buf = 0 }, { toggle = true })
+                end,
+                desc = "Toggle Current Buffer Filter",
+            },
+            s = {
+                action = function(view)
+                    local f = view:get_filter("severity")
+                    local severity = ((f and f.filter.severity or 0) + 1) % 5
+                    view:filter({ severity = severity }, {
+                        id = "severity",
+                        template = "{hl:Title}Filter:{hl} {severity}",
+                        del = severity == 0,
+                    })
+                end,
+                desc = "Toggle Severity Filter",
+            },
+        },
+        modes = {
+            symbols = {
+                desc = "document symbols",
+                mode = "lsp_document_symbols",
+                focus = false,
+                win = { position = "right" },
+                filter = {
+                    ["not"] = { ft = "lua", kind = "Package" },
+                    any = {
+                        ft = { "help", "markdown" },
+                        kind = {
+                            "Class",
+                            "Constructor",
+                            "Enum",
+                            "Field",
+                            "Function",
+                            "Interface",
+                            "Method",
+                            "Module",
+                            "Namespace",
+                            "Package",
+                            "Property",
+                            "Struct",
+                            "Trait",
+                        },
+                    },
+                },
+            },
+        },
+        icons = {
+            indent = {
+                top = "│ ",
+                middle = "├╴",
+                last = "╰╴",
+                fold_open = " ",
+                fold_closed = " ",
+                ws = "  ",
+            },
+            folder_closed = " ",
+            folder_open = " ",
+            kinds = {
+                Array = " ",
+                Boolean = "󰨙 ",
+                Class = " ",
+                Constant = "󰏿 ",
+                Constructor = " ",
+                Enum = " ",
+                EnumMember = " ",
+                Event = " ",
+                Field = " ",
+                File = " ",
+                Function = "󰊕 ",
+                Interface = " ",
+                Key = " ",
+                Method = "󰊕 ",
+                Module = " ",
+                Namespace = "󰦮 ",
+                Null = " ",
+                Number = "󰎠 ",
+                Object = " ",
+                Operator = " ",
+                Package = " ",
+                Property = " ",
+                String = " ",
+                Struct = "󰆼 ",
+                TypeParameter = " ",
+                Variable = "󰀫 ",
+            },
+        },
+    },
 
-	config = function(_, opts)
-		local trouble = require("trouble")
+    cmd = "Trouble",
 
-		trouble.setup(opts)
-
-		vim.keymap.set("n", "<leader>xx", trouble.toggle)
-
-		vim.keymap.set("n", "<leader>xw", function()
-			trouble.toggle("workspace_diagnostics")
-		end)
-
-		vim.keymap.set("n", "<leader>xd", function()
-			trouble.toggle("document_diagnostics")
-		end)
-
-		vim.keymap.set("n", "<leader>xq", function()
-			trouble.toggle("quickfix")
-		end)
-
-		vim.keymap.set("n", "<leader>xl", function()
-			trouble.toggle("loclist")
-		end)
-
-		vim.keymap.set("n", "gR", function()
-			trouble.toggle("lsp_references")
-		end)
-
-		vim.keymap.set("n", "]t", function()
-			trouble.next({ skip_groups = true, jump = true })
-		end)
-
-		vim.keymap.set("n", "[t", function()
-			trouble.previous({ skip_groups = true, jump = true })
-		end)
-	end,
+    keys = {
+        {
+            "<leader>xx",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Trouble: Diagnostics",
+        },
+        {
+            "<leader>xX",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Trouble: Buffer Diagnostics",
+        },
+        {
+            "<leader>cs",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Trouble: Symbols",
+        },
+        {
+            "<leader>cl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "Trouble: LSP Definitions / references / ...",
+        },
+        {
+            "<leader>xL",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Trouble: Location List",
+        },
+        {
+            "<leader>xQ",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Trouble: Quickfix List",
+        },
+    },
 }
