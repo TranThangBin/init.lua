ee2950c4842f483d174015bd7cf92cb6cdfe579a
TranThangBin <thangdev04@gmail.com>
2024-07-07 19:48:20 +0700

    feat: remove trailing spaces on save

--- lua/tranquangthang/init.lua
+++ lua/tranquangthang/init.lua
@@ -1,3 +1,15 @@
 require("tranquangthang.set")
 require("tranquangthang.remap")
 require("tranquangthang.lazy_init")
+
+local my_group = vim.api.nvim_create_augroup("tranquangthang", {})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	group = my_group,
+	pattern = "*",
+	callback = function()
+		local pos = vim.fn.getpos(".")
+		vim.cmd("%s/\\s\\+$//e")
+		vim.fn.setpos(".", pos)
+	end,
+})
